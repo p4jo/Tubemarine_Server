3d4ef73bc0a7b7b940e5a66709bbeebcce974dc6
Jo. H <44933755+p4jo@users.noreply.github.com>
2023-04-12 20:38:17 +0200

    generalize Server

---
+++ Server.py
@@ -0,0 +1,76 @@
+
+import time
+
+import click
+import jsonpickle
+# from serverHTTPServer import run_forever
+from serverStreams import run_forever
+from MotorSetupController import MotorSetupController
+from timetest import timeTest
+PORT = 6767  # Port to listen on (non-privileged ports are > 1023)
+
+currentReply = ""
+def addToCurrentReply(text):
+    global currentReply
+    currentReply += text
+
+motorSetupController:MotorSetupController = None
+
+@timeTest(log=lambda text: motorSetupController.current.schreiben(text, 4))
+def handle(d):
+    d = convert(d)
+    message = d.get('message','')
+    if message == 'GET_MOTOR_SETTINGS':
+        d.pop('message')
+        motorSetupController.current.schreiben('Loading current motor settings', 3)
+        motorSetupController.current.schreiben('Current motor settings: ' + str(motorSetupController.currentDict), 5)
+        return motorSetupController.currentDict
+    if message == 'SET_MOTOR_SETTINGS':
+        d.pop('message')
+        return handleMotorChange(d, False)
+    return motorSetupController.current.onReceive(d)
+
+def convert(rawData: str):    
+    try:
+        rawData = jsonpickle.decode(rawData)
+    except:
+        pass
+
+    if isinstance(rawData, str):
+        d = {'message': rawData}
+    elif isinstance(rawData, dict):
+        d = rawData
+    else:
+        d = {a: getattr(rawData, a) for a in dir(rawData) if
+            not a.startswith('__') and not callable(getattr(rawData, a))}
+    return d
+
+def handleMotorChange(obj, force = False):
+    global currentReply
+    currentReply = ""
+
+    if not isinstance(obj, dict):
+        return "The transmitted data for the new Motor config was not interpreted as a dict. Transmit json data that contains a single jso."
+
+    motorSetupController.load(obj, force=force)
+    return currentReply
+
+
+@click.command()
+@click.option("--log-level","--loglevel","-L","-l", default=6)
+@click.option('--sleep', '-s', default=0)
+def run(log_level=6, sleep=0):
+    time.sleep(sleep)
+    global motorSetupController
+    motorSetupController = MotorSetupController(log=addToCurrentReply)
+    motorSetupController.current.loglevel = log_level
+    try:
+        run_forever(PORT, motorSetupController.current.schreiben)
+    except KeyboardInterrupt:
+        print("Closed by KeyboardInterrupt")
+        if motorSetupController and motorSetupController.current:
+            motorSetupController.current.stopAndQuit()
+        exit(0)
+
+if __name__ == "__main__":
+    run()
---
+++ serverStreams.py
@@ -0,0 +1,37 @@
+import jsonpickle
+import websockets
+import asyncio
+
+async def handler(websocket):
+    while True:
+        try:
+            content = await websocket.recv()
+        except websockets.ConnectionClosedOK:
+            logFunction(f"Abgelaufene Verbindung.", 4)
+            logFunction(f"Meine Antwort wäre gewesen: {reply}", 10)
+            break
+        #############################
+        reply = handleFunction(content)
+        #############################
+        try:
+            reply_encoded = jsonpickle.encode(reply)
+            websocket.send(reply_encoded)
+        except websockets.ConnectionClosedOK:
+            break
+
+async def main(port):
+    async with websockets.serve(handler, "", port):
+        await asyncio.Future()  # run forever
+
+logFunction = print
+handleFunction = lambda text: None
+
+def run_forever(port, handle_function = None, log_function = None):
+    global logFunction, handleFunction
+    if log_function is not None:
+        logFunction = log_function
+    if handle_function is not None:
+        handleFunction = handle_function
+    for i in range(10):
+        asyncio.run(main(port))
+        log_function(f"Server died {i+1} times!")
